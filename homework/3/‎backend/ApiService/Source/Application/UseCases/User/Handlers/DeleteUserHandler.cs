5eeccd8621ddc87573b18f84d13459b4d0a1d878
gitfordevhub <lolipoppine@gmail.com>
2025-11-05 17:38:36 +0200

    Update DeleteUserHandler.cs

--- homework/3/‎backend/ApiService/Source/Application/UseCases/User/Handlers/DeleteUserHandler.cs
+++ homework/3/‎backend/ApiService/Source/Application/UseCases/User/Handlers/DeleteUserHandler.cs
@@ -1,1 +1,55 @@
 
+using CSharpFunctionalExtensions;
+using Epam.ItMarathon.ApiService.Application.UseCases.User.Commands;
+using Epam.ItMarathon.ApiService.Application.UseCases.User.Queries;
+using Epam.ItMarathon.ApiService.Domain.Abstract;
+using Epam.ItMarathon.ApiService.Domain.Shared.ValidationErrors;
+using FluentValidation.Results;
+using MediatR;
+using RoomAggregate = Epam.ItMarathon.ApiService.Domain.Aggregate.Room.Room;
+
+namespace Epam.ItMarathon.ApiService.Application.UseCases.User.Handlers
+{
+    /// <summary>
+    /// Handler for Delete User query.
+    /// </summary>
+    /// <param name="roomRepository">Implementation of <see cref="IRoomRepository"/> for operating with database.</param>
+    public class DeleteUserHandler(IRoomRepository roomRepository)
+        : IRequestHandler<DeleteUserRequest, Result<RoomAggregate, ValidationResult>>
+    {
+        ///<inheritdoc/>
+        public async Task<Result<RoomAggregate, ValidationResult>> Handle(DeleteUserRequest request,
+            CancellationToken cancellationToken)
+        {
+            // 1. Get Room by UserCode
+            var roomResult = await roomRepository.GetByUserCodeAsync(request.UserCode, cancellationToken);
+            if (roomResult.IsFailure)
+            {
+                return roomResult;
+            }
+
+            // 2. Delete User by Id in Room's Users - room DeleteUser(userID)
+            var room = roomResult.Value;
+            var deleteResult = room.DeleteUser(request.UserId);
+            if (deleteResult.IsFailure)
+            {
+                return deleteResult;
+            }
+
+            // 3. Update Room in Repository
+            var updateResult = await roomRepository.UpdateAsync(room, cancellationToken);
+            if (updateResult.IsFailure)
+            {
+                return Result.Failure<RoomAggregate, ValidationResult>(new BadRequestError([
+                    new ValidationFailure(string.Empty, updateResult.Error)
+                ]));
+            }
+
+
+            // 4. Get Updated Room
+            var updatedRoomResult = await roomRepository.GetByUserCodeAsync(request.UserCode, cancellationToken);
+
+            return updatedRoomResult;
+        }
+    }
+}
